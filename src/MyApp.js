7037dd581cf6f5bb5a2fbf841976fd7cb4c7126c
hadiasemi <hasemi@calpoly.edu>
2021-03-29 17:39:48 -0700

    Final

---
+++ src/MyApp.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+function MyApp() {
+  const [characters, setCharacters] = useState([]);  
+
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+
+  
+
+function updateList(person) {
+  setCharacters([...characters, person]);
+}
+  
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+
+
+    </div>
+  )
+
+
+}
+
+
+export default MyApp;
